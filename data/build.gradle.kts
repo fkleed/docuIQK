a226c1e5a269e507637bde5460044e7118046a5c
Ferenc Kleedörfer <93520739+fkleed@users.noreply.github.com>
2025-07-29 20:41:02 +0200

    WIP data setup. Local and prod update task

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -14,10 +14,16 @@ dependencies {
     liquibaseRuntime(libs.bundles.liquibase)
 }
 
-val dbPortLocal: Int = (project.findProperty("db.port.local") as String).toInt()
+val dbPortLocal: Int = (requireNotNull(project.findProperty("db.port.local")) as String).toInt()
 val dbNameLocal = "db"
 val dbUserLocal = "user"
 val dbPasswordLocal = "password"
+
+val dbHostProd: String = requireNotNull(project.findProperty("db.host.prod")) as String
+val dbPortProd: Int = (requireNotNull(project.findProperty("db.port.prod")) as String).toInt()
+val dbNameProd: String = requireNotNull(project.findProperty("db.name.prod")) as String
+val dbUserProd: String = requireNotNull(project.findProperty("db.user.prod")) as String
+val dbPasswordProd: String = requireNotNull(project.findProperty("db.password.prod")) as String
 
 dockerRun {
     name = "pg-gradle"
@@ -30,14 +36,52 @@ dockerRun {
 
 liquibase {
     val mainChangelog = "src/main/resources/changelog/main.yml"
-    val dbUrlLocal = "jdbc:postgresql://localhost:$dbPortLocal/$dbNameLocal?currentSchema=public&user=$dbUserLocal&password=$dbPasswordLocal"
-    activities.register("main") {
+    val dbUrlLocal = "jdbc:postgresql://localhost:$dbPortLocal/$dbNameLocal"
+    val dbUrlProd = "$dbHostProd:$dbPortProd/$dbNameProd"
+    activities.register("local") {
         this.arguments = mapOf(
-            "logLevel" to "info",
             "changelogFile" to mainChangelog,
             "url" to dbUrlLocal,
+            "username" to dbUserLocal,
+            "password" to dbPasswordLocal,
             "driver" to "org.postgresql.Driver",
+            "logLevel" to "info",
         )
     }
-    runList = "main"
+    activities.register("prod") {
+        this.arguments = mapOf(
+            "changelogFile" to mainChangelog,
+            "url" to dbUrlProd,
+            "username" to dbUserProd,
+            "password" to dbPasswordProd,
+            "driver" to "org.postgresql.Driver",
+            "logLevel" to "info",
+        )
+    }
+}
+
+val liquibaseUpdate = tasks.getByName("update")
+
+val liquibaseUpdateLocal = tasks.register("updateLocal") {
+    group = "liquibase"
+    description = "Runs Liquibase update with 'local' runList"
+
+    doFirst {
+        project.liquibase.runList = "local"
+    }
+
+    finalizedBy(liquibaseUpdate)
+
+}
+
+val liquibaseUpdateProd = tasks.register("updateProd") {
+    group = "liquibase"
+    description = "Runs Liquibase update with 'prod' runList"
+
+    doFirst {
+        project.liquibase.runList = "prod"
+    }
+
+    finalizedBy(liquibaseUpdate)
+
 }
